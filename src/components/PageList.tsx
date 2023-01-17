570db4d38ecc8762a49e1b138c147a2c09dd7ed7
callmejeje <choije401@naver.com>
2023-01-17 18:49:25 +0900

    Feat: 페이지 목록 컴포넌트

--- src/components/PageList.tsx
+++ src/components/PageList.tsx
@@ -1,30 +1,59 @@
+import { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
+import usePaginate from '../hooks/use-paginate';
+
+const PageList = () => {
+  const [searchParams] = useSearchParams();
+  const page = Number(searchParams.get('page'));
+  const { totalPage, pageArray, changePage, changePageArray } = usePaginate(page === 0 ? 1 : page);
+
+  const isSelect = useCallback(
+    (pageId: number) => {
+      if (page !== 0) return pageId === page;
+      return pageId === 1;
+    },
+    [page],
+  );
+
+  return (
+    <PageListStyle>
+      {pageArray[0] > 1 && (
+        <Page type="button" onClick={() => changePageArray('prev')} isSelect={false}>
+          이전
+        </Page>
+      )}
+      {pageArray.map((pageId) => (
+        <Page key={pageId} onClick={() => changePage(pageId)} isSelect={isSelect(pageId)}>
+          {pageId}
+        </Page>
+      ))}
+      {pageArray[pageArray.length - 1] < totalPage && (
+        <Page type="button" onClick={() => changePageArray('next')} isSelect={false}>
+          다음
+        </Page>
+      )}
+    </PageListStyle>
+  );
+};
+
+type ButtonProps = {
+  isSelect: boolean;
+};
 
 const PageListStyle = styled.div`
   margin-bottom: 20px;
   text-align: center;
 `;
 
-const Page = styled.button`
+const Page = styled.button<ButtonProps>`
   padding: 0.375rem 0.75rem;
   border-radius: 0.25rem;
   font-size: 1rem;
   line-height: 1.5;
   border: 1px solid lightgray;
   margin-right: 3px;
+  background: ${(props) => (props.isSelect ? 'gray' : 'white')};
 `;
-
-const PageList = () => {
-  // TODO : 임시로 페이지 번호 지정해두었습니다! 변경필요함
-  const pageArray = [1, 2, 3, 4, 5];
-
-  return (
-    <PageListStyle>
-      {pageArray.map((pageId) => (
-        <Page key={pageId}>{pageId}</Page>
-      ))}
-    </PageListStyle>
-  );
-};
 
 export default PageList;
