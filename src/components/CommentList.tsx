c225caac6fd37118d0979d19fed57c3ef5b60970
callmejeje <choije401@naver.com>
2023-01-18 15:32:08 +0900

    Feat: Comment 컴포넌트로 댓글 분리

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -1,14 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import styled from 'styled-components';
+import useNowPage from '../hooks/use-now-page';
 import { RootState } from '../store/config';
 import { getCommentList } from '../store/slices/commentSlice';
+import Comment from './Comment';
 
 const CommentList = () => {
   const dispatch = useDispatch();
-  const [searchParams] = useSearchParams();
-  const page = searchParams.get('page');
+  const page = useNowPage();
   const commentList = useSelector((state: RootState) => state.comments);
 
   useEffect(() => {
@@ -16,62 +15,8 @@ const CommentList = () => {
   }, [dispatch, page]);
 
   return (
-    <div>
-      {commentList &&
-        commentList.comments.map((comment) => (
-          <Comment key={comment.id}>
-            <img src={comment.profile_url} alt="" />
-
-            {comment.author}
-
-            <CreatedAt>{comment.createdAt}</CreatedAt>
-
-            <Content>{comment.content}</Content>
-
-            <Button>
-              <a>수정</a>
-              <a>삭제</a>
-            </Button>
-
-            <hr />
-          </Comment>
-        ))}
-    </div>
+    <div>{commentList && commentList.comments.map((comment) => <Comment key={comment.id} comment={comment} />)}</div>
   );
 };
-
-const Comment = styled.div`
-  padding: 7px 10px;
-  text-align: left;
-
-  & > img {
-    vertical-align: middle;
-    margin-right: 10px;
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-  }
-`;
-
-const CreatedAt = styled.div`
-  float: right;
-  vertical-align: middle;
-`;
-
-const Content = styled.div`
-  margin: 10px 0;
-`;
-
-const Button = styled.div`
-  text-align: right;
-  margin: 10px 0;
-  & > a {
-    margin-right: 10px;
-    padding: 0.375rem 0.75rem;
-    border-radius: 0.25rem;
-    border: 1px solid lightgray;
-    cursor: pointer;
-  }
-`;
 
 export default CommentList;
